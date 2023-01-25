0ab4362127e4bd3a157a27a2be48057b5c3c811e
Matheus Ramos <devmatheusr@gmail.com>
2023-01-25 11:31:57 -0300

    squares days of weeks

--- web/src/components/SumaryTable.tsx
+++ web/src/components/SumaryTable.tsx
@@ -1,4 +1,13 @@
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning"
+import { HabitDay } from "./HabitDay"
+
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
+
+const summaryDates = generateDatesFromYearBeginning()
+
+const minimumSummaryDatesSize = 18 * 7 // 18 weeks
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
+
 
 export function SumaryTable() {
     return (
@@ -12,8 +21,17 @@ export function SumaryTable() {
                 })}
             </div>
 
-            <div className="">
-                
+            <div className="grid grid-rows-7 grid-flow-col gap-3">
+                {summaryDates.map(date => {
+                    return <HabitDay key={date.toString()} />
+                })}
+
+                {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+                    return (
+                        <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
+                    )
+                })}
+
             </div>
         </div>
     )
